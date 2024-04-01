d4350b70babeddbae89a21971bbcef9bc9f5396b
Szigetvari Simon <szigetvari.simon@gmail.com>
2024-04-01 11:05:17 +0200

    testPlanID modell + Contains kapcsolat

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Test(models.Model):
     name = models.CharField(max_length=255, null=True)
     testType = models.CharField(max_length=255, null=True)
@@ -9,3 +9,13 @@ class Test(models.Model):
     deletedAt = models.DateTimeField(null=True)
 
 
+class TestPlan(models.Model):
+    name = models.CharField(max_length=255)
+    runAt = models.DateTimeField(null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    deletedAt = models.DateTimeField(null=True)
+
+
+class Contains(models.Model):
+    testID = models.ForeignKey(Test, models.CASCADE)
+    testPlanID = models.ForeignKey(TestPlan, models.CASCADE)
