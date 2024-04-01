18ef0e9d40464fe768c349afbf3de3aa19df3a94
Szigetvari Simon <szigetvari.simon@gmail.com>
2024-04-01 11:08:31 +0200

    TestPlan serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
-from .models import Test
+from .models import Test, TestPlan
+
 
 class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
-        fields = ('id', 'name', 'testType', 'implementation', 'createdAt', 'deletedAt')
+        fields = ('id', 'name', 'testType',
+                  'implementation', 'createdAt', 'deletedAt')
 
 
 class CreateTestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
         # only fields we want to declare.. fucking important comma if there is only one field -_-
-        fields = ('name', 'testType', 'implementation' )
+        fields = ('name', 'testType', 'implementation')
+
+
+class TestPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestPlan
+        fields = ('id', 'name', 'runAt', 'createdAt', 'deletedAt')
+        
+class CreateTestPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestPlan
+        fields = ('name',)
